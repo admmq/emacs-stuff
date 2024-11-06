c224e70a73e7efcd004c6c68a784381d0c1df06f
Adam <>
2024-11-06 18:30:52 +0300

    add exwm configuration

--- guix.scm
+++ guix.scm
@@ -1,32 +1,9 @@
 (use-modules (guix)
-             (guix git-download)
-             (guix build-system emacs)
-             ((guix licenses) #:prefix license:))
-
-(define vcs-file?
-  ;; Return true if the given file is under version control.
-  (or (git-predicate (current-source-directory))
-      (const #t)))
+             ((admmq pkgs emacs) #:prefix admmq:)
+             ((admmq lib) #:prefix admmq:))
 
 (package
-  (name "emacs-stuff")
-  (version "funky-version-number")
+  (inherit admmq:emacs-stuff)
   (source (local-file "." "emacs-stuff-local-build"
                       #:recursive? #t
-                      #:select? vcs-file?))
-
-  (build-system emacs-build-system)
-  (arguments
-   '(#:include '("\\.el$")
-     #:exclude '("build.el"
-                 ".dir-locals.el")
-     #:phases
-     (modify-phases %standard-phases
-       (add-after 'unpack 'load-org-files
-         (lambda _
-           (invoke
-            "emacs" "-Q" "--batch" "--load" "build.el"))))))
-  (home-page "https://github.com/admmk/emacs-stuff")
-  (synopsis "My emacs package")
-  (description "My emacs package")
-  (license license:gpl3+))
+                      #:select? admmq:vcs-file?)))
